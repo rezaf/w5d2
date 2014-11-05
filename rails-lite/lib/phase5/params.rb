03b7d3f495f9e712a0e4b0f11f235f20fea9eff7
rezaf <reza.farajian@gmail.com>
2014-11-05 12:05:00 -0800

    Mid phase IV

--- rails-lite/lib/phase5/params.rb
+++ rails-lite/lib/phase5/params.rb
@@ -9,10 +9,12 @@ module Phase5
     #
     # You haven't done routing yet; but assume route params will be
     # passed in as a hash to `Params.new` as below:
-    def initialize(req, route_params = {})
+    def initialize(req, res = nil, route_params = {})
+      @params = {req: req.query_string, res: res, route_params: route_params}
     end
 
     def [](key)
+      parse_www_encoded_form(@params[:req])[key]
     end
 
     def to_s
@@ -28,11 +30,18 @@ module Phase5
     # should return
     # { "user" => { "address" => { "street" => "main", "zip" => "89436" } } }
     def parse_www_encoded_form(www_encoded_form)
+      converted = URI::decode_www_form(www_encoded_form).to_h
+      parse_key(converted)
+      # if converted.include?("[")
+#         parse_www_encoded_form(converted)
+#       end
     end
 
     # this should return an array
     # user[address][street] should return ['user', 'address', 'street']
     def parse_key(key)
+      p key
+      key.split(/\]\[|\[|\]/)
     end
   end
 end
